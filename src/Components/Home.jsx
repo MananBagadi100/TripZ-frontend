141164f8acea9b1d66925b49b98b9cf287938545
MananBagadi100 <mananbagadi100@gmail.com>
2025-11-15 20:28:44 +0530

    Added navbar for mobile

---
+++ src/Components/Home.jsx
@@ -0,0 +1,9 @@
+import '../Styles/NavbarStyles.css'
+const Home = () => {
+    return (
+        <div className="home-full-container">
+            This is home page
+        </div>
+    )
+}
+export default Home
